933420c9f4cad5b111a890ba76fee97abf53c157
sergeybolzan <sergey.bolzan@gmail.com>
2017-05-12 12:14:09 +0300

    add timer

--- Stopwatch_Player/Stopwatch/MainWindow.xaml.cs
+++ Stopwatch_Player/Stopwatch/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Stopwatch
 {
@@ -20,9 +21,43 @@ namespace Stopwatch
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
+        TimeSpan timespanStart;
+        TimeSpan timespanEnd;
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += timer_Tick;
+            timespanStart = new TimeSpan();
+            timespanEnd = new TimeSpan();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            //timespan += timer.Interval;
+            //tbTimer.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:000}", timespan.Hours, timespan.Minutes, timespan.Seconds, timespan.Milliseconds);
+            //tbTimer.Text = DateTime.Now.ToString("HH:mm:ss.fff");
+
+            timespanEnd = DateTime.Now.TimeOfDay - timespanStart;
+            tbTimer.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:000}", timespanEnd.Hours, timespanEnd.Minutes, timespanEnd.Seconds, timespanEnd.Milliseconds);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            timespanStart = DateTime.Now.TimeOfDay;
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            lbMarks.Items.Add(timespanEnd);
         }
     }
 }
