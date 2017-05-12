1755e911986963b484c7d76ff6482f7df7dd7ab9
sergeybolzan <sergey.bolzan@gmail.com>
2017-05-12 16:40:13 +0300

    add minimize to tray

--- Stopwatch_Player/Stopwatch/MainWindow.xaml.cs
+++ Stopwatch_Player/Stopwatch/MainWindow.xaml.cs
@@ -21,12 +21,19 @@ namespace Stopwatch
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer timer;
-        TimeSpan timespanStart;
-        TimeSpan timespanEnd;
+        private System.Windows.Forms.NotifyIcon myNotifyIcon; 
+        private DispatcherTimer timer;
+        private TimeSpan timespanStart;
+        private TimeSpan timespanEnd;
         public MainWindow()
         {
             InitializeComponent();
+
+            myNotifyIcon = new System.Windows.Forms.NotifyIcon();
+            myNotifyIcon.Icon = new System.Drawing.Icon(@"D:\stopwatch.ico");
+            myNotifyIcon.MouseDoubleClick += myNotifyIcon_MouseDoubleClick;
+            this.StateChanged += MainWindow_StateChanged;
+
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(1);
             timer.Tick += timer_Tick;
@@ -34,30 +41,72 @@ namespace Stopwatch
             timespanEnd = new TimeSpan();
         }
 
+        void myNotifyIcon_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+        }
+
+        void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                myNotifyIcon.BalloonTipTitle = "Minimize Sucessful";
+                myNotifyIcon.BalloonTipText = "Minimized the app ";
+                myNotifyIcon.ShowBalloonTip(400);
+                myNotifyIcon.Visible = true;
+            }
+            else if (this.WindowState == WindowState.Normal)
+            {
+                myNotifyIcon.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        void myNotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+        }
+
+        //Обработчик таймера
         void timer_Tick(object sender, EventArgs e)
         {
-            //timespan += timer.Interval;
-            //tbTimer.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:000}", timespan.Hours, timespan.Minutes, timespan.Seconds, timespan.Milliseconds);
-            //tbTimer.Text = DateTime.Now.ToString("HH:mm:ss.fff");
-
             timespanEnd = DateTime.Now.TimeOfDay - timespanStart;
             tbTimer.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:000}", timespanEnd.Hours, timespanEnd.Minutes, timespanEnd.Seconds, timespanEnd.Milliseconds);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //Нажатие на кнопку "ЗАПУСК"
+        private void btnStart_Click(object sender, RoutedEventArgs e)
         {
+            lbMarks.Items.Clear();
+            btnStopReset.Content = "СТОП";
             timer.Start();
             timespanStart = DateTime.Now.TimeOfDay;
+            btnLap.IsEnabled = true;
         }
 
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        //Нажатие на кнопку "СТОП (СБРОС)"
+        private void btnStopReset_Click(object sender, RoutedEventArgs e)
         {
-            timer.Stop();
+            btnLap.IsEnabled = false;
+            if (timer.IsEnabled)
+            {
+                btnStopReset.Content = "СБРОС";
+                timer.Stop();
+            }
+            else
+            {
+                btnStopReset.Content = "СТОП";
+                tbTimer.Text = "00:00:00:000";
+                lbMarks.Items.Clear();
+            }
         }
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+
+        //Нажатие на кнопку "КРУГ"
+        private void btnLap_Click(object sender, RoutedEventArgs e)
         {
-            lbMarks.Items.Add(timespanEnd);
+            string newStringForListBox = String.Format("{0}-й круг! Время: {1:00}:{2:00}:{3:00}:{4:000}", lbMarks.Items.Count + 1, timespanEnd.Hours, timespanEnd.Minutes, timespanEnd.Seconds, timespanEnd.Milliseconds);
+            lbMarks.Items.Add(newStringForListBox);
         }
     }
 }
