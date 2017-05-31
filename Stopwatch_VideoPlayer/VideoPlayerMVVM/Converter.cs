1860f26eb3d71bd0e83cc36dcd7ca93a6f73da2f
sergeybolzan <sergey.bolzan@gmail.com>
2017-05-31 17:21:23 +0300

    add mvvm_project

---
+++ Stopwatch_VideoPlayer/VideoPlayerMVVM/Converter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VideoPlayerMVVM
+{
+    public class Converter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Path.GetFileNameWithoutExtension((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Stopwatch_VideoPlayer/VideoPlayerMVVM/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace VideoPlayerMVVM
+{ 
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
