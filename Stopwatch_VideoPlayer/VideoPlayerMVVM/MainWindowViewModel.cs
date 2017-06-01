2d30fe308886d561c10660105691fc8ae5fb9f9d
sergeybolzan <sergey.bolzan@gmail.com>
2017-06-01 18:04:46 +0300

    update

--- Stopwatch_VideoPlayer/VideoPlayerMVVM/MainWindowViewModel.cs
+++ Stopwatch_VideoPlayer/VideoPlayerMVVM/MainWindowViewModel.cs
@@ -27,20 +27,48 @@ namespace VideoPlayerMVVM
             get
             {
                 return openFiles ?? (openFiles = new RelayCommand(obj =>
+                {
+                    OpenFileDialog openFileDialog = OpenFileDialog();
+                    if (openFileDialog.ShowDialog() == true)
                     {
-                        OpenFileDialog openFileDialog = new OpenFileDialog();
-                        openFileDialog.InitialDirectory = Environment.CurrentDirectory;
-                        openFileDialog.Multiselect = true;
-                        openFileDialog.Filter = "Video files(*.mp4;*.mkv;*.wmv;*.avi)|*.mp4;*.mkv;*.wmv;*.avi|All files (*.*)|*.*";
-                        if (openFileDialog.ShowDialog() == true)
+                        PlayList.Clear();
+                        foreach (var item in openFileDialog.FileNames)
                         {
-                            foreach (var item in openFileDialog.FileNames)
-                            {
-                                PlayList.Add(item);
-                            }
+                            PlayList.Add(item);
                         }
-                    }));
+                    }
+                }));
             }
+        }
+        private RelayCommand addFilesToPlayList;
+        public RelayCommand AddFilesToPlayList
+        {
+            get
+            {
+                return addFilesToPlayList ?? (addFilesToPlayList = new RelayCommand(obj =>
+                {
+                    OpenFileDialog openFileDialog = OpenFileDialog();
+                    if (openFileDialog.ShowDialog() == true)
+                    {
+                        foreach (var item in openFileDialog.FileNames)
+                        {
+                            PlayList.Add(item);
+                        }
+                    }
+                }));
+            }
+        }
+        /// <summary>
+        /// Creating a customized instance of OpenFileDialog
+        /// </summary>
+        /// <returns></returns>
+        private OpenFileDialog OpenFileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog.Filter = "Video files(*.mp4;*.mkv;*.wmv;*.avi)|*.mp4;*.mkv;*.wmv;*.avi|All files (*.*)|*.*";
+            openFileDialog.Multiselect = true;
+            return openFileDialog;
         }
     }
 }
