e93a156fa46fac5534e6ea16d23606ceaaf24d93
sergeybolzan <sergey.bolzan@gmail.com>
2017-08-24 18:00:55 +0300

    add studentsApp

---
+++ StudentsAppTW/StudentsAppTW/Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsAppTW.Model
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public bool Gender { get; set; }
+    }
+}
