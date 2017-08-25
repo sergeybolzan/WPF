42b345a76bf3f330cc864bbd6d98879c47ce3f84
sergeybolzan <sergey.bolzan@gmail.com>
2017-08-25 16:48:45 +0300

    upd

--- StudentsAppTW/StudentsAppTW/Model/Student.cs
+++ StudentsAppTW/StudentsAppTW/Model/Student.cs
@@ -6,12 +6,119 @@ using System.Threading.Tasks;
 
 namespace StudentsAppTW.Model
 {
-    class Student
+    //class Student
+    //{
+    //    public int Id { get; set; }
+    //    public string Firstname { get; set; }
+    //    public string Lastname { get; set; }
+    //    public int Age { get; set; }
+    //    public bool Gender { get; set; }
+    //}
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class Students
     {
-        public int Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public int Age { get; set; }
-        public bool Gender { get; set; }
+
+        private StudentsStudent[] studentField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Student")]
+        public StudentsStudent[] Student
+        {
+            get
+            {
+                return this.studentField;
+            }
+            set
+            {
+                this.studentField = value;
+            }
+        }
     }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class StudentsStudent
+    {
+
+        private string firstNameField;
+
+        private string lastField;
+
+        private byte ageField;
+
+        private byte genderField;
+
+        private byte idField;
+
+        /// <remarks/>
+        public string FirstName
+        {
+            get
+            {
+                return this.firstNameField;
+            }
+            set
+            {
+                this.firstNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Last
+        {
+            get
+            {
+                return this.lastField;
+            }
+            set
+            {
+                this.lastField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte Age
+        {
+            get
+            {
+                return this.ageField;
+            }
+            set
+            {
+                this.ageField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte Gender
+        {
+            get
+            {
+                return this.genderField;
+            }
+            set
+            {
+                this.genderField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte Id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+    }
+
+
 }
